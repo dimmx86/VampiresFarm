385ff826e839337ad47f4d5e14b27c7474425303
dimmx86 <dimmx86@gmail.com>
2023-08-26 14:02:12 +0300

    CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private float maxDeathZone = 1f;
+    [SerializeField] private float speed = 1;
+
+    private Vector3 ofssetPosition;
+    private bool isMove = false;
+    private Vector3 targetPosition;
+
+    private void Start()
+    {
+        ofssetPosition = transform.position - targetTransform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        targetPosition = targetTransform.position + ofssetPosition;
+
+        if (isMove)
+        {
+            Move();
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, targetPosition) > maxDeathZone)
+            {
+                isMove = true;
+            }
+        }
+    }
+
+
+    private void Move()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position,targetPosition) < 0.1f)
+        {
+            isMove = false;
+        }
+    }
+}
